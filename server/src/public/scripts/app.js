67148620e10a7dadd4c9bb8f1572eb8c2bb12661
blair B <blair441035@gmail.com>
2020-07-15 23:31:13 +0900

    test

--- server/src/public/scripts/app.js
+++ server/src/public/scripts/app.js
@@ -4,6 +4,3 @@ import { createElWithClass } from './utils/createElWithClass';
 // SCSS import
 import '../scss/mainPage.scss';
 import '../scss/reset.scss';
-
-const body = document.querySelector('.list-header-left-wrap');
-body.appendChild(createElWithClass('div', 'test', '안녕하세요'));
