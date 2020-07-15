6a99eb228f941e53090dbe4179fd60e628286d4f
blair B <blair441035@gmail.com>
2020-07-15 20:39:58 +0900

    feature:mainPage markup

--- server/src/public/scripts/app.js
+++ server/src/public/scripts/app.js
@@ -1,16 +1,17 @@
-import '../scss/app.scss';
-import sum from './math';
-import baeminImage from '../img/baemin.png';
+import '../scss/mainPage.scss';
+import '../scss/reset.scss';
+// import sum from './math';
+// import baeminImage from '../img/baemin.png';
 // import './app.scss';
-import card from './components/card';
+// import card from './components/card';
 
-window.addEventListener('DOMContentLoaded', () => { // DOM 생성 후 실행
-    const el = document.querySelector('#app');
-    el.innerHTML = `<h1>1 + 2 = ${sum()}</h1>
-    <img src="${baeminImage}" alt="woowa" />`;
-
-    const h1 = document.querySelector('h1');
-    console.log(card());
-    h1.appendChild(card());
-});
-
+// window.addEventListener('DOMContentLoaded', () => { // DOM 생성 후 실행
+//     const el = document.querySelector('#app');
+//     el.innerHTML = `<h1>1 + 2 = ${sum()}</h1>
+//     <img src="${baeminImage}" alt="woowa" />`;
+//
+//     const h1 = document.querySelector('h1');
+//     console.log(card());
+//     h1.appendChild(card());
+// });
+//
