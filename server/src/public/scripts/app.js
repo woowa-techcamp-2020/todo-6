7fd8392921428442b658d7521843165a8abaf65b
blair B <blair441035@gmail.com>
2020-07-16 13:58:18 +0900

    feature:create card section

--- server/src/public/scripts/app.js
+++ server/src/public/scripts/app.js
@@ -1,6 +1,39 @@
 // JS import
-import { createElWithClass } from './utils/createElWithClass';
+import newCardSectionEl from './components/list/createCardArea';
 
 // SCSS import
 import '../scss/mainPage.scss';
 import '../scss/reset.scss';
+
+const cardsWrap = document.querySelector('.cards-wrap');
+const listBodySection = document.querySelector('.list-body-section');
+listBodySection.insertBefore(newCardSectionEl, cardsWrap);
+
+
+
+// const initData = [
+//     {
+//         listId: 'number',
+//         listName: 'string',
+//         cardsCount: 'number',
+//         cards: [
+//             {
+//                 cardName: 'string',
+//                 cardText: 'string',
+//                 createTime: 'timeObj',
+//             },
+//         ],
+//     },
+//     {
+//         listId: 'number',
+//         listName: 'string',
+//         cardsCount: 'number',
+//         cards: [
+//             {
+//                 cardName: 'string',
+//                 cardText: 'string',
+//                 createTime: 'timeObj',
+//             },
+//         ],
+//     },
+// ];
