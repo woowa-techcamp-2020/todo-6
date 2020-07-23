a27201c1a9b5e1fa73da9159cd9ae2cb7d90755e
myungwoo-Y <auddn6676@naver.com>
2020-07-23 17:00:40 +0900

    feature: drag event #27 - 드래그 시 이벤트 추가 - 이벤트 최신순으로 불러오기

--- client/scripts/controller/dragHandler.js
+++ client/scripts/controller/dragHandler.js
@@ -1,59 +1,86 @@
 import {
-    addEventToMenu, eventType, eventTypeID, getListOrdersObj, setElementPos,
+    addEventToMenu, eventType, eventTypeID, getCardText, getListOrdersObj, getListText, setElementPos,
 } from '../utils/handleElement';
 import { elements } from '../utils/states';
 import { putUpdateCard, putUpdateOrder } from '../apis';
 import changedList from '../utils/changedList';
 
-export function elementToDraggable(element, x, y) {
-    let differX = 0; let differY = 0; let curX = 0; let
-        curY = 0;
-    curX = x;
-    curY = y;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-
-    elements.hoverParentCard.classList.add('under-hover-card');
-
-    function elementDrag(event) {
-        event.preventDefault();
-
-        differX = curX - event.clientX;
-        differY = curY - event.clientY;
-        curX = event.clientX;
-        curY = event.clientY;
-
-        const newTop = element.offsetTop - differY;
-        const newLeft = element.offsetLeft - differX;
-        setElementPos(element, newTop, newLeft);
+export default class ElementToDraggable {
+    constructor(hoverCard, x, y) {
+        this.hoverCard = hoverCard;
+        this.beforeListID = hoverCard.dataset.listid;
+        this.differX = 0;
+        this.differY = 0;
+        this.curX = 0;
+        this.curY = 0;
+        this.curX = x;
+        this.curY = y;
+        document.onmouseup = this.closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = this.elementDrag;
+        elements.hoverParentCard.classList.add('under-hover-card');
     }
 
-    function removeHoverInfoInElements() {
+    elementDrag = (event) => {
+        event.preventDefault();
+
+        this.differX = this.curX - event.clientX;
+        this.differY = this.curY - event.clientY;
+        this.curX = event.clientX;
+        this.curY = event.clientY;
+
+        const newTop = this.hoverCard.offsetTop - this.differY;
+        const newLeft = this.hoverCard.offsetLeft - this.differX;
+        setElementPos(this.hoverCard, newTop, newLeft);
+    }
+
+    removeHoverInfoInElements() {
         elements.hoverCard = null;
         elements.constructor = null;
         elements.hoverParentCard.classList.remove('under-hover-card');
     }
 
-    function closeDragElement() {
+    updateEvnet(list, hoverParentCard) {
+        if (this.beforeListID !== this.listID) {
+            const cardText = getCardText(hoverParentCard);
+            const listTitle = getListText(list);
+            const beforeList = document.querySelector(`[data-listid='${this.beforeListID}']`);
+            const befreListTitle = getListText(beforeList);
+
+            addEventToMenu({
+                userID: 1,
+                id: 'auddn6676',
+                eventTypeID: eventTypeID.moveCard,
+                card: cardText,
+                list: listTitle,
+                beforeList: befreListTitle,
+                typeName: eventType.moveCard,
+            });
+        }
+    }
+
+    closeDragElement = () => {
         document.onmouseup = null;
         document.onmousemove = null;
-        element.remove();
+        this.hoverCard.remove();
 
         try {
             const { hoverParentCard } = elements;
 
             const list = hoverParentCard.closest('.list');
             const listID = list.dataset.listid;
-            hoverParentCard.dataset.listid = listID;
+            this.listID = listID;
+            hoverParentCard.dataset.listid = this.listID;
             changedList.addChangedListsToState(list);
             changedList.updateCardsOrder();
             putUpdateCard({
                 listID,
                 cardID: hoverParentCard.dataset.cardid,
             });
+            this.updateEvnet(list, hoverParentCard);
         } catch (e) {
+            console.log(e);
         }
-        removeHoverInfoInElements();
+        this.removeHoverInfoInElements();
     }
 }
